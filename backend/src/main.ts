c0db37580197bcd791487d464a63de522e57afcc
Priyadharshini M <priyadharshinimurugesan29@gmail.com>
2026-08-21 17:21:26 +0530

    Prepare backend for deployment

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,10 +5,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: true,
   });
 
-  await app.listen(3001);
+  const port = process.env.PORT || 3001;
+
+  await app.listen(port);
 }
 
 bootstrap();
